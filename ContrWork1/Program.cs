7d656cc00b90bd91a3497577ec9e4910a72c977e
Gulempinchenko Maksim <gulempin@gmail.com>
2022-12-06 11:57:25 +0300

    complet method 1

---
+++ ContrWork1/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] NewTextArray(string[] array1)
+{
+    string[] arrayText2 = new string[3];
+    for (int i = 0, j = 0; i < array1.Length; i++, j++)
+    {
+    if(array1[i].Length <= arrayText2.Length)
+        {
+        arrayText2[j] = array1[i];
+        }
+       
+    }
+    return arrayText2;
+}
